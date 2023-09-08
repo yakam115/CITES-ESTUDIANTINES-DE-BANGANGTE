acf3f2c6a0998daf1fee1067605d9146ff378c67
yakam115 <111244560+yakam115@users.noreply.github.com>
2023-09-08 16:51:25 +0100

    Add files via upload

---
+++ data/campusuniversitairesdeBangangte_4.js
@@ -0,0 +1,1 @@
+var json_campusuniversitairesdeBangangte_4 = {"type":"FeatureCollection","name":"campusuniversitairesdeBangangte_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
